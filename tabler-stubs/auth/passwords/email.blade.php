459dc0a7870978a5ca96ef52021beec96488a39d
John Astete <john.astete.arambarr@gmail.com>
2020-10-12 20:59:00 -0300

    update reset email

--- tabler-stubs/auth/passwords/email.blade.php
+++ tabler-stubs/auth/passwords/email.blade.php
@@ -1,74 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ config('app.name') }}</title>
 
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-          crossorigin="anonymous"/>
-
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>{{ config('app.name') }}</b></a>
-    </div>
 
-    <!-- /.login-logo -->
-    <div class="card">
-        <div class="card-body login-card-body">
-            <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
-
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            <form action="{{ route('password.email') }}" method="post">
-                @csrf
-
-                <div class="input-group mb-3">
-                    <input type="email"
-                           name="email"
-                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                           placeholder="Email">
-                    <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-envelope"></span></div>
-                    </div>
-                    @if ($errors->has('email'))
-                        <span class="error invalid-feedback">{{ $errors->first('email') }}</span>
-                    @endif
-                </div>
-
+<body class="antialiased">
+    <div class="page">
+        <div class="page-single">
+            <div class="container-xl">
                 <div class="row">
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary btn-block">Send Password Reset Link</button>
+                    <div class="col-lg-4 offset-lg-4 col-login ">
+                        <div class="text-center mt-6 mb-4">
+                            <img src="https://preview-dev.tabler.io/static/logo.svg" class="h-5" alt="">
+                        </div>
+                        <form class="card" action="{{ route('password.email') }}" method="post">
+                            @csrf
+                            <div class="card-body p-lg-5 p-md-2">
+                                <div class="card-title">Forgot password</div>
+                                <div class="card-subtitle">Enter your email address and your password will be reset and emailed to you.</div>
+
+                                @if (session('status'))
+                                <div class="alert alert-success">
+                                    {{ session('status') }}
+                                </div>
+                                @endif
+
+                                <div class="form-group">
+                                    <label class="form-label">Email address</label>
+                                    <input type="email" name="email" value="{{ old('email') }}" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" aria-describedby="email" placeholder="Enter email">
+                                    @error('email')
+                                    <span class="error invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-footer">
+                                    <button type="submit" class="btn btn-primary btn-block">Send me new password</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="text-center text-muted">
+                            Forget it, <a href="{{ route('login') }}">send me back</a> to the sign in screen.
+                        </div>
                     </div>
-                    <!-- /.col -->
                 </div>
-            </form>
-
-            <p class="mt-3 mb-1">
-                <a href="{{ route("login") }}">Login</a>
-            </p>
-            <p class="mb-0">
-                <a href="{{ route("register") }}" class="text-center">Register a new membership</a>
-            </p>
+            </div>
         </div>
-        <!-- /.login-card-body -->
     </div>
-</div>
-<!-- /.login-box -->
 
-<script src="{{ mix('js/app.js') }}" defer></script>
-
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
+
 </html>
