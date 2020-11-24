e8e0bdc53746d04f24635fc1801cd89f77a0d218
John Astete <john.astete.arambarri@gmail.com>
2020-11-23 21:16:23 -0300

    updates login and register

--- tabler-stubs/auth/login.blade.php
+++ tabler-stubs/auth/login.blade.php
@@ -10,52 +10,78 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class="antialiased">
-    <div class="page">
-        <div class="page-single">
-            <div class="container-xl">
-                <div class="row">
-                    <div class="col-lg-4 offset-lg-4 col-login ">
-                        <div class="text-center mt-6 mb-4">
-                            <img src="https://preview-dev.tabler.io/static/logo.svg" class="h-5" alt="">
+<body class="antialiased border-top-wide border-primary d-flex flex-column">
+    <div class="flex-fill d-flex flex-column justify-content-center py-4">
+        <div class="container-tight py-6">
+            <div class="text-center mb-4">
+                <a href="."><img src="https://preview-dev.tabler.io/static/logo.svg" height="36" alt=""></a>
+            </div>
+            <form class="card card-md" action="{{ url('/login') }}" method="post" autocomplete="off">
+                @csrf
+                <div class="card-body">
+                    <h2 class="card-title text-center mb-4">Login to your account</h2>
+
+                    <div class="mb-3">
+                        <label class="form-label">Email address</label>
+                        <input type="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" placeholder="Enter email">
+
+                        @error('email')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-2">
+                        <label class="form-label">
+                            Password
+                            <span class="form-label-description">
+                                <a href="{{ route('password.request') }}">I forgot password</a>
+                            </span>
+                        </label>
+                        <div class="input-group input-group-flat">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" autocomplete="off">
                         </div>
-                        <form class="card" action="{{ url('/login') }}" method="post">
-                            @csrf
-                            <div class="card-body p-lg-5 p-md-2">
-                                <div class="card-title">Login to your account</div>
-                                <div class="form-group">
-                                    <label class="form-label">Email address</label>
-                                    <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" aria-describedby="email" placeholder="Enter email">
-                                    @error('email')
-                                    <span class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label class="form-label">
-                                        Password
-                                        <a href="{{ route('password.request') }}" class="float-right small">I forgot password</a>
-                                    </label>
-                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
-                                    @error('password')
-                                    <span class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" />
-                                        <span class="custom-control-label">Remember me</span>
-                                    </label>
-                                </div>
-                                <div class="form-footer">
-                                    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                                </div>
-                            </div>
-                        </form>
-                        <div class="text-center text-muted">
-                            Don't have account yet? <a href="{{ route('register') }}">Sign up</a>
+
+                        @error('password')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-2">
+                        <label class="form-check">
+                            <input type="checkbox" class="form-check-input" />
+                            <span class="form-check-label">Remember me on this device</span>
+                        </label>
+                    </div>
+                    <div class="form-footer">
+                        <button type="submit" class="btn btn-primary w-100">Sign in</button>
+                    </div>
+                </div>
+                <div class="hr-text">or</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <a href="#" class="btn btn-white w-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon text-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                                </svg>
+                                Login with Github
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a href="#" class="btn btn-white w-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon text-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+                                </svg>
+                                Login with Twitter
+                            </a>
                         </div>
                     </div>
                 </div>
+            </form>
+            <div class="text-center text-muted mt">
+                Don't have account yet? <a href="{{ route('register') }}" tabindex="-1">Sign up</a>
             </div>
         </div>
     </div>
--- tabler-stubs/auth/register.blade.php
+++ tabler-stubs/auth/register.blade.php
@@ -10,73 +10,65 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class="antialiased">
-    <div class="page">
-        <div class="page-single">
-            <div class="container-xl">
-                <div class="row">
-                    <div class="col-lg-4 offset-lg-4 col-login ">
-                        <div class="text-center mt-6 mb-4">
-                            <img src="https://preview-dev.tabler.io/static/logo.svg" class="h-5" alt="">
-                        </div>
-                        <form class="card" action="{{ route('register') }}" method="post">
-                            @csrf
-                            <div class="card-body p-lg-5 p-md-2">
-                                <div class="card-title">Create new account</div>
+<body class="antialiased border-top-wide border-primary d-flex flex-column">
+    <div class="flex-fill d-flex flex-column justify-content-center py-4">
+        <div class="container-tight py-6">
+            <div class="text-center mb-4">
+                <a href="."><img src="https://preview-dev.tabler.io/static/logo.svg" height="36" alt=""></a>
+            </div>
+            <form class="card card-md" action="{{ route('register') }}" method="post">
+                @csrf
+                <div class="card-body">
+                    <h2 class="card-title text-center mb-4">Create new account</h2>
+                    <div class="mb-3">
+                        <label class="form-label">Name</label>
+                        <input type="text" name="name" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror" placeholder="Enter name">
 
-                                <div class="form-group">
-                                    <label class="form-label">Name</label>
-                                    <input type="text" name="name" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror" aria-describedby="name" placeholder="Full name">
-                                    @error('name')
-                                    <span class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
+                        @error('name')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
 
-                                <div class="form-group">
-                                    <label class="form-label">Email address</label>
-                                    <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" aria-describedby="email" placeholder="Enter email">
-                                    @error('email')
-                                    <span class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
+                    <div class="mb-3">
+                        <label class="form-label">Email address</label>
+                        <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" placeholder="Enter email">
 
-                                <div class="form-group">
-                                    <label class="form-label">
-                                        Password
-                                    </label>
-                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
-                                    @error('password')
-                                    <span class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
+                        @error('email')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
 
-                                <div class="form-group">
-                                    <label class="form-label">
-                                        Password
-                                    </label>
-                                    <input type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror" placeholder="Retype password">
-                                    @error('password')
-                                    <span class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" autocomplete="off">
 
-                                <div class="form-group">
-                                    <label class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" name="terms" value="agree" />
-                                        <span class="custom-control-label">I agree the <a href="#">terms and policy</a></span>
-                                    </label>
-                                </div>
+                        @error('password')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
 
-                                <div class="form-footer">
-                                    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                                </div>
-                            </div>
-                        </form>
-                        <div class="text-center text-muted">
-                            Already have account? <a href="{{ route('login') }}">Sign in</a>
-                        </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror" placeholder="Retype password" autocomplete="off">
+
+                        @error('password')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-check">
+                            <input type="checkbox" class="form-check-input" />
+                            <span class="form-check-label">Agree the <a href="./terms-of-service.html" tabindex="-1">terms and policy</a>.</span>
+                        </label>
+                    </div>
+                    <div class="form-footer">
+                        <button type="submit" class="btn btn-primary w-100">Create new account</button>
                     </div>
                 </div>
+            </form>
+            <div class="text-center text-muted mt">
+                Already have account? <a href="{{ route('login') }}" tabindex="-1">Sign in</a>
             </div>
         </div>
     </div>
